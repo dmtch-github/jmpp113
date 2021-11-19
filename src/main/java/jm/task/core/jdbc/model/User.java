71f93697822a766f5723e98ee09aa6d21febc292
dmtch-github <dmtch@yandex.ru>
2021-11-19 20:02:58 +0300

    Метод получения Connect сделал статическим. Упростил код.

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,63 +1,34 @@
 package jm.task.core.jdbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
 
-@Table
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
+@NoArgsConstructor
+@Setter
+@Getter
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
 
-    @Column
+    @Column(name="name")
     private String name;
 
-    @Column
+    @Column(name="lastname")
     private String lastName;
 
-    @Column
+    @Column(name="age")
     private Byte age;
 
-    public User() {
-
-    }
-
-    public User(Long id, String name, String lastName, Byte age) {
-        this.id = id;
+    public User(String name, String lastName, Byte age) {
         this.name = name;
         this.lastName = lastName;
-        this.age = age;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Byte getAge() {
-        return age;
-    }
-
-    public void setAge(Byte age) {
         this.age = age;
     }
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,59 +1,58 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Util {
-    private final Logger log = Logger.getLogger(Class.class.getName());
-    public final static boolean flagMysql = true;
-    private final String DB_DRIVER_NAME;
-    private final String DB_URL;
-    private final String DB_USER_NAME;
-    private final String DB_USER_PASSWORD;
+    private static final Logger log = Logger.getLogger(Util.class.getName());
+    public static final boolean FLAG_USE_MYSQL = false;
+    public static final String SQL_CMD_CREATE_TABLE;
+    private static final String DB_DRIVER_NAME;
+    private static final String DB_URL;
+    private static final String DB_USER_NAME;
+    private static final String DB_USER_PASSWORD;
 
-    private Connection connection = null;
-
-    public Util() {
-        if(flagMysql) {
-            log.info("Работаю с БД MySQL");
+    static {
+        if(FLAG_USE_MYSQL) {
             DB_DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
             DB_URL = "jdbc:mysql://localhost:3306/mydbtest";
             DB_USER_NAME = "root";
             DB_USER_PASSWORD = "rootroot";
+            SQL_CMD_CREATE_TABLE = "CREATE TABLE users (id INT NOT NULL AUTO_INCREMENT,name VARCHAR(45) NOT NULL,lastName VARCHAR(45) NOT NULL,age INT(3) NOT NULL,PRIMARY KEY (id), UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE);";
         } else {
-            log.info("Работаю с БД PostgreSQL");
             DB_DRIVER_NAME = "org.postgresql.Driver";
             DB_URL = "jdbc:postgresql://localhost:5432/mydbtest";
             DB_USER_NAME = "admin";
             DB_USER_PASSWORD = "12345678";
+            SQL_CMD_CREATE_TABLE = "CREATE TABLE users (id SERIAL PRIMARY KEY, name CHARACTER VARYING(30) NOT NULL, lastName CHARACTER VARYING(30) NOT NULL, Age INTEGER NOT NULL);";
         }
     }
 
-    public Connection getConnection() {
-        if(connection == null) {
-            try {
-                Class.forName(DB_DRIVER_NAME);
-                connection = DriverManager.getConnection(DB_URL, DB_USER_NAME, DB_USER_PASSWORD);
-            } catch (ClassNotFoundException | SQLException e) {
-                e.printStackTrace();
-            }
+    private Util() {
+
+    }
+
+    public static Connection getConnectionJDBC() {
+        Connection connection = null;
+        try {
+            Class.forName(DB_DRIVER_NAME);
+            connection = DriverManager.getConnection(DB_URL, DB_USER_NAME, DB_USER_PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
         }
         return connection;
     }
 
-    //Вопрос: как здесь правильно обрабатывать исключения?
-    //Передавать выше, если Да, то каким классом? RuntimeException?
-    public void closeConnection() {
-        if(connection != null) {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } finally {
-                connection = null;
-            }
+    public static void closeConnectionJDBC(Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException | NullPointerException e) {
+            log.warning("При работе closeConnectionJDBC возникла ошибка " + e);
         }
     }
 }
